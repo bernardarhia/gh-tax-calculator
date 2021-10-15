49219a095e643bede211f2780e7dd1442cfe8497
Bernard Arhia <bernardarhia@gmail.com>
2021-10-15 22:26:03 +0000

    enabled cors origin to all

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors")
 require("dotenv/config")
 const corsOptions = {
     credentials:true,
-    origin:"http://localhost:3000"
+    origin:"*"
 }
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
